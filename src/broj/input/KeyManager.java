e85ceff41778837111ca078b87f2342cf5a6ee3e
Uros Stojiljkovic <urosss007@gmail.com>
2018-09-05 14:09:11 +0200

    Podrzan input sa numpada

--- src/broj/input/KeyManager.java
+++ src/broj/input/KeyManager.java
@@ -14,16 +14,16 @@ public class KeyManager implements KeyListener {
     }
     
     public void tick() {
-        numberKeys[0] = keys[KeyEvent.VK_0];
-        numberKeys[1] = keys[KeyEvent.VK_1];
-        numberKeys[2] = keys[KeyEvent.VK_2];
-        numberKeys[3] = keys[KeyEvent.VK_3];
-        numberKeys[4] = keys[KeyEvent.VK_4];
-        numberKeys[5] = keys[KeyEvent.VK_5];
-        numberKeys[6] = keys[KeyEvent.VK_6];
-        numberKeys[7] = keys[KeyEvent.VK_7];
-        numberKeys[8] = keys[KeyEvent.VK_8];
-        numberKeys[9] = keys[KeyEvent.VK_9];
+        numberKeys[0] = keys[KeyEvent.VK_0] || keys[KeyEvent.VK_NUMPAD0];
+        numberKeys[1] = keys[KeyEvent.VK_1] || keys[KeyEvent.VK_NUMPAD1];
+        numberKeys[2] = keys[KeyEvent.VK_2] || keys[KeyEvent.VK_NUMPAD2];
+        numberKeys[3] = keys[KeyEvent.VK_3] || keys[KeyEvent.VK_NUMPAD3];
+        numberKeys[4] = keys[KeyEvent.VK_4] || keys[KeyEvent.VK_NUMPAD4];
+        numberKeys[5] = keys[KeyEvent.VK_5] || keys[KeyEvent.VK_NUMPAD5];
+        numberKeys[6] = keys[KeyEvent.VK_6] || keys[KeyEvent.VK_NUMPAD6];
+        numberKeys[7] = keys[KeyEvent.VK_7] || keys[KeyEvent.VK_NUMPAD7];
+        numberKeys[8] = keys[KeyEvent.VK_8] || keys[KeyEvent.VK_NUMPAD8];
+        numberKeys[9] = keys[KeyEvent.VK_9] || keys[KeyEvent.VK_NUMPAD9];
         
         Arrays.fill(keys, false);
     }
